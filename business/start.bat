a8b10e70cbd314c2787bf9b0eecb96559570b4fa
Ernesto <brunoeca23@gmail.com>
2022-10-12 10:47:59 -0500

    command line

--- business/start.bat
+++ business/start.bat
@@ -8,5 +8,7 @@ echo 45000	12	0.20	365	12	30	>business.csv
 
 
 
-echo 45000,120.20,365	 > a.txt
+echo 45000	12	19	0.20	40	35	25	20	15	8	40	52	365	 > a.txt
 echo -,-,-,-,-,-, >> a.txt
+echo welome >> a.txt
+echo name	rate >> a.txt
