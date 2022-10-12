8cfee4ca95ccf55f8c27033d1b85b55dfa9ca9e6
Ernesto <brunoeca23@gmail.com>
2022-10-12 10:55:59 -0500

    redone

--- business/start.bat
+++ business/start.bat
@@ -12,3 +12,10 @@ echo 45000	12	19	0.20	40	35	25	20	15	8	40	52	365	 > a.txt
 echo -,-,-,-,-,-, >> a.txt
 echo welome >> a.txt
 echo name	rate >> a.txt
+echo business	-45000 >> a.txt
+echo business	-35 >> a.txt
+echo business	-25 >> a.txt
+echo business	-20 >> a.txt
+echo business	-45000 >> a.txt
+echo business	-45000 >> a.txt
+echo business	-45000 >> a.txt
