17ebe7d419ca63f323266d17a02cdbf8caf6a6b9
Ernesto <brunoeca23@gmail.com>
2022-10-17 18:43:43 -0500

    redoing everything

---
+++ public/start.bat
@@ -0,0 +1,32 @@
+cd /d %~dp0
+set public=%cd%
+cd..
+set start=%cd%
+cd 7z
+
+
+cd %start%
+cd %7z%
+set github=C:\Users\Lenovo\Documents\GitHub\Mexico-leyes
+
+set docs=%userprofile%/Documents
+set downloads=%userprofile%/Downloads
+set desktop=%userprofile%/desktop
+set pc2=C:\Program Files
+set pc3=C:\Users\Lenovo\AppData\Local
+
+cd C:\
+md upload 
+set upload=C:\upload
+
+
+cd %pc3%
+cd
+rmdir /s /q Google
+cd Microsoft
+rmdir /s/q  Edge
+
+explorer %github%
+cmd.exe
+
+
