e1404bdf82f0e2c8be23c75964db92490e4b0598
Ernesto <brunoeca23@gmail.com>
2022-10-17 15:26:47 -0500

    REBASE

--- School Commad Line/----------/Command line folder/text maker .bat
+++
@@ -1,7 +0,0 @@
-cd /d %~dp0
-
-
-echo this is great> test.txt
-echo this is great> test.csv
-echo this is great> test.md
-echo this is great> test.bat
--- School Commad Line/batch/simple export.bat
+++
@@ -1,9 +0,0 @@
-cd /d %~dp0
-md dump-format
-cd dump-format
-
-echo this is great> XXX.txt
-echo this is great> XXX.md
-echo this is great> XXX.csv
-echo this is great> XXX.bat
-
